9b2e950eea6841e77bf9c030e4e52a585a0e27cb
Karan Sharma <hello@mrkaran.dev>
2026-04-01 12:24:17 +0530

    fix: shorten slog source to file:line via ReplaceAttr

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,22 +2,32 @@
 package logger
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
 )
 
-// New creates a new logger with the specified level
-// If component is provided, it will be added as a "component" attribute
+// New creates a new logger with the specified level.
+// Source is logged as "source":"file.go:123" (short form).
 func New(debug bool) *slog.Logger {
 	logLevel := slog.LevelInfo
 	if debug {
 		logLevel = slog.LevelDebug
 	}
 
-	// Create handler with appropriate level
 	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level:     logLevel,
 		AddSource: true,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// Shorten source from full path to "file.go:line"
+			if a.Key == slog.SourceKey {
+				if src, ok := a.Value.Any().(*slog.Source); ok {
+					a.Value = slog.StringValue(fmt.Sprintf("%s:%d", filepath.Base(src.File), src.Line))
+				}
+			}
+			return a
+		},
 	})
 
 	return slog.New(handler)
